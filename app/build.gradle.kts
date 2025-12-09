0c1cb5d1e93d35071ff174503abf922d1e3c576c
Upuaut <mastiffzone365@gmail.com>
2025-12-09 23:47:11 +0800

    ui changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.xposedsearch"
+    namespace = "com.upuaut.xposedsearch"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.xposedsearch"
+        applicationId = "com.upuaut.xposedsearch"
         minSdk = 26
         targetSdk = 35
         versionCode = 1
